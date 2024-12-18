3499b1d1421e4e3074463a8fbebb14a78135dc61
KimGyeongyeon <kgy556@naver.com>
2024-12-18 20:44:43 +0900

    feat: add Spring project

---
+++ api/src/test/kotlin/com/kims/playground/interest/calcurator/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kims.playground.interest.calcurator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
