3b29ac702830eefd937f79415694d2b65225f953
KimGyeongyeon <kgy556@naver.com>
2024-12-18 23:49:14 +0900

    feat: server handles calculation

--- api/src/main/kotlin/com/kims/playground/interest/calculator/InterestController.kt
+++ api/src/main/kotlin/com/kims/playground/interest/calculator/InterestController.kt
@@ -3,12 +3,10 @@ package com.kims.playground.interest.calculator
 import com.kims.playground.interest.calculator.convert.dto.RequestInterestDto
 import com.kims.playground.interest.calculator.convert.dto.ResponseInterestDto
 import com.kims.playground.interest.calculator.convert.service.SavingsToDepositService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = ["*"])
 class InterestController(
     val service: SavingsToDepositService
 ) {
@@ -17,8 +15,15 @@ class InterestController(
         @RequestParam monthlySaving: Long,
         @RequestParam durationMonth: Int,
         @RequestParam interestPercent: Double
-    ): ResponseInterestDto {
-        val requestInterestDto = RequestInterestDto(monthlySaving, durationMonth, interestPercent)
-        return service.convert(requestInterestDto)
+    ): ResponseInterestDto? {
+        println("GET yearly-interest 요청 수신")
+        try{
+            val requestInterestDto = RequestInterestDto(monthlySaving, durationMonth, interestPercent)
+            val response = service.convert(requestInterestDto)
+            return response
+        } catch (e: Exception) {
+            println(e.message)
+            return null
+        }
     }
 }
